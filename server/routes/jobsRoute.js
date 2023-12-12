2c0fafe8a210e5e807c5542d8dc404e42bd9563a
Darshi Buddhini <120660394+DarshiBuddhini@users.noreply.github.com>
2023-12-12 19:30:34 +0530

    updated backend for job portal

--- server/routes/jobsRoute.js
+++ server/routes/jobsRoute.js
@@ -1,103 +1,84 @@
 const express = require("express");
 const router = express.Router();
-const Jobs = require('../models/jobsModel')
+const Jobs = require("../models/jobsModel");
 
-//add new jobs
-router.post("/post/jobs", async (req, res) => {
+router.post("/", async (req, res) => {
+  const { jobtitle, category, ministry, description, salary, location } =
+    req.body;
 
-    const { jobtitle,category, description ,salary,location} = req.body
-    try {
-
-        // Creating a new job object and saving it in database
-
-        const newJobs = new Jobs({ jobtitle,category, description ,salary,location })
-        newJobs.save()
-        res.send('Job posted successfully!')
-
-
-    } catch (error) {
-
-        return res.status(400).json({ message: error });
-    }
+  try {
+    // Creating a new job object and saving it in the database
+    const newJobs = new Jobs({
+      jobtitle,
+      category,
+      ministry,
+      description,
+      salary,
+      location,
+    });
+    await newJobs.save(); // Add await here
+    res.status(201).json({ message: "Job posted successfully!" });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: error.message });
+  }
 });
 
 //get all job details
-router.get("/getalljobs", async (req, res) => {
-
-    try {
-
-        const jobs = await Jobs.find({})
-        res.send(jobs)
-
-    } catch (error) {
-        return res.status(400).json({ message: error });
-    }
+router.get("/", async (req, res) => {
+  try {
+    const jobs = await Jobs.find({});
+    res.send(jobs);
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
 });
 
 //update newsfeed
-router.put("/update/jobs/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
+  let userId = req.params.id;
+  const { jobtitle, category, description, salary, location } = req.body;
 
-    let userId = req.params.id;
-    const {jobtitle,category, description ,salary,location } = req.body;
+  const updateJobs = {
+    jobtitle,
+    category,
+    description,
+    salary,
+    location,
+  };
 
-    const updateJobs = {
+  try {
+    await Jobs.findByIdAndUpdate(userId, updateJobs);
+    res.send("Job details updated successfully!");
+  } catch (error) {
+    // Returning error message if any error occurs during job update
 
-        jobtitle, 
-        category, 
-        description,
-        salary,
-        location 
-    }
-
-    try {
-
-        await Jobs.findByIdAndUpdate(userId, updateJobs)
-        res.send('Job details updated successfully!')
-
-    } catch (error) {
-                // Returning error message if any error occurs during job update
-
-        return res.status(400).json({ message: error });
-    }
+    return res.status(400).json({ message: error });
+  }
 });
 
-router.delete("/delete/jobs/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  let JobID = req.params.id;
 
-    let JobID = req.params.id;
+  try {
+    await Jobs.findByIdAndDelete(JobID);
 
-    try {
-        await Jobs.findByIdAndDelete(JobID)
-
-        res.send('Job Deleted Successfully')
-    }
-
-    catch (error) {
-
-
-        return res.status(400).json({ message: error });
-    }
+    res.send("Job Deleted Successfully");
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
 });
 
 //get current jobs
-router.get("/getcurrentjobs/:id", async (req, res) => {
-
-    let jobsId = req.params.id;
-    try {
-
-        const currentjobs = await Jobs.findById(jobsId)
-        res.send(currentjobs)
-
-    } catch (error) {
-        return res.status(400).json({ message: error });
-    }
-
-})
-
-
-
-
-
-
+router.get("/:id", async (req, res) => {
+  let jobsId = req.params.id;
+  try {
+    const currentjobs = await Jobs.findById(jobsId);
+    res.send(currentjobs);
+  } catch (error) {
+    return res.status(400).json({ message: error });
+  }
+});
 
 // Exporting router module for use in other files
 
