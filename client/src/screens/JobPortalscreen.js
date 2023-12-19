4890d2413ab6ab34ffe223d82316b9c38bc94394
Darshi Buddhini <120660394+DarshiBuddhini@users.noreply.github.com>
2023-12-19 17:03:13 +0530

    Update JobPortalscreen.js

--- client/src/screens/JobPortalscreen.js
+++ client/src/screens/JobPortalscreen.js
@@ -3,27 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 import Jobs from "../components/Jobs";
-import { getAllJobs } from './../actions/jobportalAction';
-import { getAllJobsReducer } from './../reducers/jobsReducer';
-
-
+import { getAllJobs } from "./../actions/jobportalAction";
+import { getAllJobsReducer } from "./../reducers/jobsReducer";
 
 function JobPortalscreen() {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const jobsstate = useSelector(state => state.getAllJobsReducer)
-  const [currentFeedbacks, setFeedbacks] = useState([]);
-  const { jobs, error, loading } = jobsstate
+  const jobsstate = useSelector((state) => state.getAllJobsReducer);
+  const { jobs, error, loading } = jobsstate;
 
   useEffect(() => {
-    dispatch(getAllJobs())
-  }, [])
-
+    dispatch(getAllJobs());
+  }, []);
 
   return (
     <div>
-
       <br />
       <br />
       <br />
@@ -33,20 +27,25 @@ function JobPortalscreen() {
           <br />
           <br />
           <br />
-          <h10 style={{ fontSize: "45px", color: "white" }}>
-            Career Portal
+          <h1 style={{ color: "white" }}>
+            Government Career Portal
             {/* <h1>Current Time: {time}</h1> */}
-          </h10>
-          <p style={{ fontSize: "14px", color: "white" }}>“Example”</p>
+          </h1>
+          <p style={{ fontSize: "14px", color: "white" }}>
+            Government Job Vacancies & Gazzetes
+          </p>
           <br />
         </div>
       </div>
 
-
-      {loading ? (<Loading />) : error ? (<Error error='Something went wrong' />) : (
-        <div className='d-flex flex-wrap justify-content-center'>
-          {jobs.map(jobs => (
-            <div className='col-md-3 m-3' key={jobs._id}>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Error error="Something went wrong" />
+      ) : (
+        <div className="d-flex flex-wrap justify-content-center">
+          {jobs.map((jobs) => (
+            <div className="col-md-3 m-3" key={jobs._id}>
               <div>
                 <Jobs jobs={jobs} />
               </div>
@@ -54,11 +53,8 @@ function JobPortalscreen() {
           ))}
         </div>
       )}
-
     </div>
-
-
-  )
+  );
 }
 
-export default JobPortalscreen
+export default JobPortalscreen;
