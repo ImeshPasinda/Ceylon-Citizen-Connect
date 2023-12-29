945da0fe12fe2ec465866a8e5d40b734f7cd1e70
Darshi Buddhini <120660394+DarshiBuddhini@users.noreply.github.com>
2023-12-29 11:32:54 +0530

    changed electricity backend

--- server/models/electricityUserModel.js
+++ server/models/electricityUserModel.js
@@ -1,25 +1,14 @@
 const mongoose = require("mongoose");
 
 const electricityBillSchema = new mongoose.Schema({
-  accountName: { type: String, required: true },
+  accountNo: { type: String, required: true },
+  email: { type: String, required: true },
   month: { type: String, required: true },
-  year:{ type: String, required: true },
+  date:{ type: String, required: true },
   totalUnits: { type: Number, required: true },
-  monthlyBill: { type: Number, required: true },
+  monthlyElecBill: { type: Number, required: true },
 });
 
-const electricityUserSchema = mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    electricityMeterNumber: { type: String, required: true, unique: true },
-    electricityBills: [electricityBillSchema], // Array of e-bills
 
-  },
-  {
-    timestamps: true,
-  }
-);
 
-module.exports = mongoose.model("ElectricityUser", electricityUserSchema);
+module.exports = mongoose.model("ElectricityBill", electricityBillSchema);
