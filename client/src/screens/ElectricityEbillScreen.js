3667f2f97c247e5f67556c52b90c0cf8972066a6
Imesh Pasinda <imeshpasinda@gmail.com>
2023-12-30 15:53:00 +0530

    Finalizing Functions v1.4

--- client/src/screens/ElectricityEbillScreen.js
+++ client/src/screens/ElectricityEbillScreen.js
@@ -153,9 +153,7 @@ export default function ElectricityEbillScreen() {
                         </div>
                     </div>
                 </div>
-                <div className="d-flex justify-content-center" style={{ paddingTop: '20px' }}>
-
-                    <div className="row justify-content-center">
+                <div className="row justify-content-center" style={{ paddingTop: '20px' }}>
                         {ElectricitybillData.map((ElectricitybillItem, index) => (
                             <div key={index} className="col-12">
                                 <div className="card card shadow p-0 bg-white rounded justify-content-center w-100 mb-3"> {/* Adjusted card width to 100% */}
@@ -188,8 +186,10 @@ export default function ElectricityEbillScreen() {
                             </div>
                         ))}
                     </div>
-                </div>
             </div>
+
+
+
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="offcanvasExampleLabel">
