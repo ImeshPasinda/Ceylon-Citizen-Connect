4bf26df44d3dffd07568ea96a9194d6bbc200c57
Imesh Pasinda <imeshpasinda@gmail.com>
2024-01-08 07:25:12 +0530

    Updated moment-timezone package

--- server/bots/botLogClearBot/botLogClearBot.js
+++ server/bots/botLogClearBot/botLogClearBot.js
@@ -2,19 +2,36 @@ const BotLog = require("../../models/botLogModal")
 const cron = require('node-cron');
 const { BotLogClearBotCronConfig } = require("../../botCronConfig");
 
-
 cron.schedule(BotLogClearBotCronConfig.scheduleRate, async () => {
     try {
         const count = await BotLog.countDocuments();
 
         if (count > 10) {
-            await BotLog.deleteMany({});
-            console.log('Deleted all Bot Logs.');
+            const deleteResult = await BotLog.deleteMany({});
+            console.log(`Deleted ${deleteResult.deletedCount} Bot Logs.`);
+
+            const ranDate = moment.tz('Asia/Kolkata').format('YYYY-MM-DD hh:mm:ss A');
+
+            const botLogData = {
+                botName: 'BotLogClearBot',
+                log: `Deleted ${deleteResult.deletedCount} Bot Logs.`,
+                ranDate: ranDate,
+            };
+            const insertedBotLog = await BotLog.create(botLogData);
+            console.log('Bot log added after clearing Bot Logs:', insertedBotLog);
         } else {
             console.log('Bot Logs count is not greater than 10.');
         }
     } catch (err) {
-        console.error('Error:', err);
+        const ranDateError = moment.tz('Asia/Kolkata').format('YYYY-MM-DD hh:mm:ss A');
+
+        const errorBotLogData = {
+            botName: 'BotLogClearBot',
+            log: `BotLogClearBot Failed: ${err.message}`,
+            ranDate: ranDateError,
+        };
+        const insertedErrorBotLog = await BotLog.create(errorBotLogData);
+        console.log('Error added to the bot log:', insertedErrorBotLog);
     }
 }, {
     timezone: BotLogClearBotCronConfig.timezone
